6db9bb56221371e6ca6968108e5ae3ad19b29f56
raspi <pekka.jarvinen@gmail.com>
2021-01-24 14:08:23 +0200

    init

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"github.com/raspi/fs2util/pkg/vp"
+	"hash"
+	"os"
+	"path"
+	"sort"
+	"strings"
+)
+
+var (
+	// These are set with Makefile -X=main.VERSION, etc
+	VERSION   = `v0.0.0`
+	BUILD     = `dev`
+	BUILDDATE = `0000-00-00T00:00:00+00:00`
+)
+
+const (
+	AUTHOR   = `Pekka Järvinen`
+	HOMEPAGE = `https://github.com/raspi/fs2util`
+)
+
+var checksummers = map[string]hash.Hash{
+	"sha256": sha256.New(),
+	"sha1":   sha1.New(),
+	"md5":    md5.New(),
+	"sha512": sha512.New(),
+}
+
+func main() {
+
+	extractArg := flag.Bool(`x`, false, `Extract files`)
+	extractDirArg := flag.String(`edir`, `extracted`, `Extract directory name`)
+
+	var csumAlgos []string
+	for aname, _ := range checksummers {
+		csumAlgos = append(csumAlgos, aname)
+	}
+	sort.Strings(csumAlgos)
+
+	checksumArg := flag.Bool(`c`, false, `Calculate checksums inside the file`)
+	csumAlgoArg := flag.String(`algo`, `sha256`, `Checksum algorithm: `+strings.Join(csumAlgos, `, `))
+
+	infoArg := flag.Bool(`i`, false, `Display information about files inside the .vp file`)
+
+	flag.Usage = func() {
+		f := os.Args[0]
+		_, _ = fmt.Fprintf(os.Stdout, `fs2util - Inspect FreeSpace 2 .VP file`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `(c) %v 2019- [ %v ]`+"\n", AUTHOR, HOMEPAGE)
+		_, _ = fmt.Fprintf(os.Stdout, "\n")
+
+		_, _ = fmt.Fprintf(os.Stdout, "Parameters:\n")
+
+		paramMaxLen := 0
+
+		flag.VisitAll(func(f *flag.Flag) {
+			l := len(f.Name)
+			if l > paramMaxLen {
+				paramMaxLen = l
+			}
+		})
+
+		flag.VisitAll(func(f *flag.Flag) {
+			padding := strings.Repeat(` `, paramMaxLen-len(f.Name))
+			_, _ = fmt.Fprintf(os.Stdout, "  -%s%s   %s   default: %q\n", f.Name, padding, f.Usage, f.DefValue)
+		})
+
+		_, _ = fmt.Fprintf(os.Stdout, "\n")
+
+		_, _ = fmt.Fprintf(os.Stdout, `List file checksums:`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `  %s -c <file.vp>`+"\n", f)
+		_, _ = fmt.Fprintf(os.Stdout, `Extract file(s):`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `  %s -x <file.vp>`+"\n", f)
+		_, _ = fmt.Fprintf(os.Stdout, "\n")
+		_, _ = fmt.Fprintf(os.Stdout, `Examples:`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `  Show checksums:`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `    %s -c root_fs2.vp`+"\n", f)
+		_, _ = fmt.Fprintf(os.Stdout, `  Extract files:`+"\n")
+		_, _ = fmt.Fprintf(os.Stdout, `    %s -x root_fs2.vp`+"\n", f)
+	}
+
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if !*extractArg && !*checksumArg && !*infoArg {
+		_, _ = fmt.Fprintf(os.Stderr, `missing parameter(s), see --help`)
+		os.Exit(1)
+	}
+
+	filename := flag.Arg(flag.NArg() - 1)
+
+	fi, err := os.Stat(filename)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, `Not file: %q`, filename)
+		os.Exit(1)
+	}
+
+	if !fi.Mode().IsRegular() {
+		_, _ = fmt.Fprintf(os.Stderr, `Not file: %q`, fi.Name())
+		os.Exit(1)
+	}
+
+	hasher, ok := checksummers[*csumAlgoArg]
+
+	if !ok {
+		_, _ = fmt.Fprintf(os.Stderr, `Not valid algo: %q`, *csumAlgoArg)
+		os.Exit(1)
+	}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	reader := vp.New(f, hasher)
+
+	err = reader.ReadFile()
+	if err != nil {
+		panic(err)
+	}
+
+	// Get files
+	for _, fi := range reader.GetFiles() {
+
+		fInfo, err := f.Stat()
+		if err != nil {
+			panic(err)
+		}
+
+		if *infoArg {
+			fmt.Printf(`source:%s file:%s/%s size:%d bytes, offset:%d date:%v`+"\n", fInfo.Name(), fi.Path, fi.FileName, fi.FileSize, fi.Offset, fi.Time.UTC())
+		}
+
+		if *extractArg {
+			// Extract file
+			expath, err := reader.ExtractFile(fi, path.Join(*extractDirArg, fInfo.Name()))
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf(`Extracted file %s/%s size:%d bytes, offset:%d (%v) to %s`+"\n", fi.Path, fi.FileName, fi.FileSize, fi.Offset, fi.Time.UTC(), expath)
+		}
+
+		if *checksumArg {
+			// Get file checksum
+			checksum, err := reader.ChecksumFile(fi)
+			if err != nil {
+				panic(err)
+			}
+
+			fmt.Printf(`%s  %s/%s/%s`+"\n", checksum, fInfo.Name(), fi.Path, fi.FileName)
+		}
+	}
+
+}
---
+++ pkg/vp/inspector.go
@@ -0,0 +1,187 @@
+package vp
+
+import (
+	"encoding/binary"
+	"fmt"
+	"hash"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+	"time"
+)
+
+type RawHeader struct {
+	Magic           [4]byte // VPVP
+	Version         uint32  // 2
+	DirectoryOffset uint32
+	EntryCount      uint32
+}
+
+type RawFileEntry struct {
+	Offset        uint32
+	Size          uint32
+	FileName      [32]byte // null-terminated
+	UnixTimeStamp uint32
+}
+
+type File struct {
+	Path     string
+	FileName string
+	FileSize int
+	Offset   int64
+	Time     time.Time
+}
+
+func (rfe RawFileEntry) GetFileName() string {
+	n := string(rfe.FileName[:])
+	return n[0:strings.IndexByte(n, 0)]
+}
+
+type Inspector struct {
+	f           io.ReadSeeker
+	header      RawHeader
+	files       []File
+	checksummer hash.Hash
+}
+
+func New(f io.ReadSeeker, checksummer hash.Hash) Inspector {
+	return Inspector{
+		f:           f,
+		checksummer: checksummer,
+	}
+}
+
+func (i *Inspector) ReadFile() error {
+	err := binary.Read(i.f, binary.LittleEndian, &i.header)
+	if err != nil {
+		return err
+	}
+
+	hdr := string(i.header.Magic[:])
+	if hdr != `VPVP` {
+		return fmt.Errorf(`invalid header: %q`, hdr)
+	}
+
+	if i.header.Version != 2 {
+		return fmt.Errorf(`invalid version: %d`, i.header.Version)
+	}
+
+	return i.readDirectoryIndex()
+}
+
+func (i *Inspector) readDirectoryIndex() error {
+	// Go to Directory Index offset
+	_, err := i.f.Seek(int64(i.header.DirectoryOffset), io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	fpath := `.`
+
+	// Read directory index file entries
+	for idx := 0; idx < int(i.header.EntryCount); idx++ {
+		var fe RawFileEntry
+		err = binary.Read(i.f, binary.LittleEndian, &fe)
+		if err != nil {
+			panic(err)
+		}
+
+		if fe.Size == 0 {
+			// Size 0 = directory
+			fpath = path.Join(fpath, fe.GetFileName())
+			continue
+		}
+
+		i.files = append(i.files, File{
+			Path:     fpath,
+			FileName: fe.GetFileName(),
+			FileSize: int(fe.Size),
+			Offset:   int64(fe.Offset),
+			Time:     time.Unix(int64(fe.UnixTimeStamp), 0),
+		})
+	}
+
+	return nil
+
+}
+
+func (i Inspector) GetFiles() []File {
+	return i.files
+}
+
+// Extract file
+func (i Inspector) ExtractFile(target File, extractionDir string) (string, error) {
+	_, err := i.f.Seek(target.Offset, io.SeekStart)
+	if err != nil {
+		return ``, err
+	}
+
+	buffer := make([]byte, target.FileSize)
+
+	_, err = i.f.Read(buffer)
+	if err != nil {
+		return ``, err
+	}
+
+	// Write to temporary file
+	err = os.MkdirAll(extractionDir, os.ModePerm)
+	if err != nil {
+		return ``, err
+	}
+	f, err := ioutil.TempFile(extractionDir, `extracted-`)
+	if err != nil {
+		return ``, err
+	}
+
+	// Write contents to file
+	_, err = f.Write(buffer)
+	if err != nil {
+		return ``, err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return ``, err
+	}
+
+	newPath := path.Join(extractionDir, target.Path)
+	err = os.MkdirAll(newPath, os.ModePerm)
+	if err != nil {
+		return ``, err
+	}
+
+	newFilePath := path.Join(newPath, target.FileName)
+
+	// Rename temporary file to proper filename
+	err = os.Rename(f.Name(), newFilePath)
+	if err != nil {
+		return ``, err
+	}
+
+	return newFilePath, nil
+}
+
+// Calculate checksum
+func (i Inspector) ChecksumFile(target File) (string, error) {
+	_, err := i.f.Seek(target.Offset, io.SeekStart)
+	if err != nil {
+		return ``, err
+	}
+
+	buffer := make([]byte, target.FileSize)
+
+	_, err = i.f.Read(buffer)
+	if err != nil {
+		return ``, err
+	}
+
+	i.checksummer.Reset()
+	_, err = i.checksummer.Write(buffer)
+	if err != nil {
+		return ``, err
+	}
+
+	return fmt.Sprintf(`%x`, i.checksummer.Sum(nil)), nil
+}
